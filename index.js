24e00459bf06a6c5e82f115d3e87aa8323cdc5be
smooooky <75350537+smooooky@users.noreply.github.com>
2021-12-14 17:49:36 -0500

    added environment variables

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 8888;
 
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URL = process.env.REDIRECT_URL;
+
+// console.log(process.env.CLIENT_ID);
 
 app.get('/',(req,res) => {
     res.send('Hello World!');
