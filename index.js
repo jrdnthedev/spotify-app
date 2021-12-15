350d310b59f689f23c4d1cb4a37a4e571b2c8032
smooooky <75350537+smooooky@users.noreply.github.com>
2021-12-15 00:38:24 -0500

    added react front end, started setup to pass tokens to front end

--- index.js
+++ index.js
@@ -66,19 +66,16 @@ app.get('/callback',(req,res) => {
         },
     }).then(response => {
         if(response.status === 200) {
-            const { access_token, token_type } = response.data;
-            //use access token to request data from api
-            axios.get('https://api.spotify.com/v1/me',{
-                headers: {
-                    Authorization: `${token_type} ${access_token}`
-                }
-            }).then(response => {
-                res.send(`<pre>${JSON.stringify(response.data, null, 2)}</pre>`);
-            }).catch(error => {
-                res.send(error);
+            const { access_token, refresh_token } = response.data;
+            const queryParams = querystring.stringify({
+                access_token,
+                refresh_token
             })
+            //redirect to react app
+            res.redirect(`http://localhost:3000/?${queryParams}`);
+            //pass along tokens in query params
         } else {
-            res.send(response);
+            res.redirect(`/?${querystring.stringify({error:'invalid token value'})}`);
         }
     }).catch(error => {
         res.send(error);
