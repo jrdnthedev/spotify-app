d23b5b87a54a56f03ae98b0e075c4c30c62a90c9
smooooky <75350537+smooooky@users.noreply.github.com>
2021-12-20 18:46:45 -0500

    added user profile header and added new route to get playlists for user

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,24 @@ import {
 import {accessToken, logout, getCurrentUserProfile} from './spotify';
 import {catchErrors} from './utils';
 import { GlobalStyle } from './styles';
-import { Login } from './pages';
+import { Login, Profile } from './pages';
+import styled from 'styled-components/macro';
+
+const StyledLogoutButton = styled.button`
+  position: absolute;
+  top: var(--spacing-sm);
+  right: var(--spacing-md);
+  padding: var(--spacing-xs) var(--spacing-sm);
+  background-color: rgba(0,0,0,.7);
+  color: var(--white);
+  font-size: var(--fz-sm);
+  font-weight: 700;
+  border-radius: var(--border-radius-pill);
+  z-index: 10;
+  @media (min-width: 768px) {
+    right: var(--spacing-lg);
+  }
+`;
 
 // Scroll to top of page when changing routes
 // https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
@@ -45,6 +62,8 @@ function App() {
         {!token ?(
           <Login />
         ): (
+          <>
+          <StyledLogoutButton onClick={logout}>Log Out</StyledLogoutButton>       
           <Router>
             <ScrollToTop />
             <Switch>
@@ -61,22 +80,11 @@ function App() {
                 <h1>Playlists</h1>
               </Route>
               <Route path="/">
-                <>
-                  <button onClick={logout}>Log Out</button>
-
-                  {profile && (
-                    <div>
-                      <h1>{profile.display_name}</h1>
-                      <p>{profile.followers.total} Followers</p>
-                      {profile.images.length && profile.images[0].url && (
-                        <img src={profile.images[0].url} alt="Avatar"/>
-                      )}
-                    </div>
-                  )}
-                </>
+                <Profile />
               </Route>
             </Switch>
           </Router>
+          </>
         )}  
       </header>
     </div>
